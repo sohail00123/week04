95ac547a31705a38ec0aa9449e29bd6d4dfbd64c
sohail00123 <20043369@mydbs.ie>
2024-10-11 13:37:49 +0100

    added script

---
+++ script.js
@@ -0,0 +1,29 @@
+
+alert("script loaded")
+function calculateTotalCost(basket, prices) {
+    let totalCost = 0;
+
+    for (const product in basket) {
+        if (prices[product]) {
+            totalCost += basket[product] * prices[product];
+        }
+    }
+
+    return totalCost;
+}
+
+// Example usage:
+const basket = {
+    apples: 2,
+    bananas: 3,
+    oranges: 1
+};
+
+const prices = {
+    apples: 1.5,
+    bananas: 0.5,
+    oranges: 2
+};
+
+const total = calculateTotalCost(basket, prices);
+console.log(total); 
